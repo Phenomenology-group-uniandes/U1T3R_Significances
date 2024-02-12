31a1615d7090fe234ee4577d5bf12037ba3cde97
Cristian Fernando Rodríguez Cruz <94081091+cfrc2694@users.noreply.github.com>
2024-02-12 12:12:39 +0000

    ADD Data exploration

---
+++ load_data.py
@@ -0,0 +1,27 @@
+# Load the data
+import numpy as np
+import os
+
+xu_masses = [500, 750, 1000, 1500, 2000]  # GeV
+phi_masses = [1, 5, 10, 50, 100, 325]  # GeV
+mc_names = ["signal", "ttbarmumu", "ttbarmumumunu"]
+
+data_path = os.path.join(os.getcwd(), "data")
+print("Loading data from", data_path)
+
+data = {
+    "xu_{}_phi_{}".format(xu_mass, phi_mass): {
+        f"{mc}": np.load(
+            os.path.join(
+                data_path,
+                f"xu_{xu_mass}_GeV",
+                f"phi_{phi_mass}_GeV",
+                f"{mc}.npy",
+            )
+        )
+        for mc in mc_names
+    }
+    for xu_mass in xu_masses
+    for phi_mass in phi_masses
+}
+print("Done!")
